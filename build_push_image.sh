181dc3a03ce62758407f108fc168f422f9b4ef21
aprianfirlanda <aprianfirlanda@gmail.com>
2024-01-31 00:53:44 +0700

    fix sh build push image

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,4 +1,5 @@
 docker build -t item-app:v1 .
-echo $PASSWORD_DOCKER_HUB | docker login -u aprianfirlanda --password-stdin
+docker image list
 docker tag item-app:v1 aprianfirlanda/item-app:v1
+echo $PASSWORD_DOCKER_HUB | docker login -u aprianfirlanda --password-stdin
 docker push aprianfirlanda/item-app:v1
