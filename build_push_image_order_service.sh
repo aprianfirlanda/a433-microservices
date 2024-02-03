c2992d1a0dadd4b4d585f70e8f86547ddd480bf1
aprianfirlanda <aprianfirlanda@gmail.com>
2024-02-03 17:25:03 +0700

    add comment on shell script build push image

--- build_push_image_order_service.sh
+++ build_push_image_order_service.sh
@@ -1,3 +1,6 @@
+# build image dengan versi latest
 docker build -t aprianfirlanda/order-service:latest .
+# login ke docker hub
 echo $PASSWORD_DOCKER_HUB | docker login -u aprianfirlanda --password-stdin
+# push image ke docker hub
 docker push aprianfirlanda/order-service:latest
